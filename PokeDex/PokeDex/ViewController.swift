a90488d18c5c85405bc50676681b6e83ec1faec5
Eymen Varilci <evarilci@gmail.com>
2023-03-23 17:04:47 +0300

    Initial commit

---
+++ PokeDex/PokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Eymen Varilci on 23.03.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
